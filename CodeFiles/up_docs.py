2e9fa3ebf34ebb95573bb58c3e4b571c49f7e2eb
Yaswant Sai Krishna <48371142+YaswantSaiKrishna@users.noreply.github.com>
2021-01-11 09:10:55 +0530

    Update up_docs.py

--- CodeFiles/up_docs.py
+++ CodeFiles/up_docs.py
@@ -11,7 +11,7 @@ def upload_docs(Data_Frame, index_name, endpoint, key):
     client = SearchClient(endpoint=endpoint,
                           index_name=index_name,
                           credential=credential)
-    df = pd.read_csv('/TrackerFiles/{}.csv'.format(index_name))
+    df = pd.read_csv('TrackerFiles/{}.csv'.format(index_name))
     i = df.shape[0] + 1
     for e in dataframe2:
         DOCUMENT = {
